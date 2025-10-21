dd2249c92c6bde9eb3cd3b5ee7cffad29e4768a5
damianocastellarin <damianocastellarin@gmail.com>
2025-10-21 14:26:08 +0200

    add decrement button

--- tally_counter/lib/main.dart
+++ tally_counter/lib/main.dart
@@ -37,6 +37,12 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
+
   void _halveCounter() {
     setState(() {
       _counter ~/= 2;
@@ -71,19 +77,28 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          //halve
           FloatingActionButton(
             onPressed: _halveCounter,
-            tooltip: 'Halve', // dimezzare
+            tooltip: 'Halve',
             child: Text('Halve'),
           ),
-          const SizedBox(width: 16),
+          //decre
+          const SizedBox(width: 20),
+          FloatingActionButton(
+            onPressed: _decrementCounter,
+            tooltip: 'Decrement',
+            child: const Icon(Icons.remove),
+          ),
+          //incre
+          const SizedBox(width: 20),
           FloatingActionButton(
             onPressed: _incrementCounter,
-            tooltip: 'Increment', 
+            tooltip: 'Increment',
             child: const Icon(Icons.add),
           ),
-
-          const SizedBox(width: 16), //raddoppia
+          //raddoppia
+          const SizedBox(width: 20),
           FloatingActionButton(
             onPressed: _doubleCounter,
             tooltip: 'Double',
