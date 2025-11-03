643033cfe4510213c8ad1f4f0f641469cb9efaf7
damianocastellarin <damianocastellarin@gmail.com>
2025-11-03 22:17:37 +0100

    add toggleSwitch and randomColor packages from pub.dev

--- bgcolor_changer/lib/main.dart
+++ bgcolor_changer/lib/main.dart
@@ -1,7 +1,6 @@
-import 'dart:math';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_randomcolor/flutter_randomcolor.dart';
+import 'package:toggle_switch/toggle_switch.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,33 +9,53 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Esercizio 1.3',
+      title: 'Cambio Colore',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Background Color Changer'),
+      home: const ColorChangerPage(title: 'Cambio Colore'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-
+class ColorChangerPage extends StatefulWidget {
+  const ColorChangerPage({super.key, required this.title});
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<ColorChangerPage> createState() => _ColorChangerPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-   Color _sfondo = Colors.white;
-  bool light0 = true;
+class _ColorChangerPageState extends State<ColorChangerPage> {
+  Color _backgroundColor = Colors.white;
+  bool _isYellow = true;
 
+  void _changeColor(Color newColor) {
+    setState(() {
+      _backgroundColor = newColor;
+    });
+  }
+
+  void _resetColor() {
+    setState(() {
+      _backgroundColor = Colors.white;
+    });
+  }
+
+  void _randomColor() {
+    setState(() {
+      _backgroundColor = RandomColor.getColorObject(
+        Options(
+          colorType: ColorType.random,
+          luminosity: Luminosity.light,
+          format: Format.rgb,
+        ),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -45,133 +64,93 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: Column(
-        children: [
-          Expanded( 
-            child: Container(
-              color:_sfondo,
-              child: Center(
-                child: Column(
+
+      body: AnimatedContainer(
+        duration: const Duration(milliseconds: 500),
+        color: _backgroundColor,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(height: 20),
+              Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                Text('Gioca con i colori!', style: TextStyle(fontSize: 20),),
-                Text('Qua puoi scegliere se visualizzare la schermata in "Light" o in "Dark" mode.'),
-                Switch(
-                  value: light0,
-                  onChanged: (bool value) {
-                    setState(() {
-                      _switch();
-                    });
-                  },
-                ),
-                  ],
-                )
-                
+                  // button rosso
+                  FloatingActionButton(
+                    backgroundColor: Colors.red,
+                    onPressed: () => _changeColor(Colors.red),
+                  ),
+                  const SizedBox(width: 20),
+                  // button verde
+                  FloatingActionButton(
+                    backgroundColor: Colors.green,
+                    onPressed: () => _changeColor(Colors.green),
+                  ),
+                  const SizedBox(width: 20),
+                  // button blu
+                  FloatingActionButton(
+                    backgroundColor: Colors.blue,
+                    onPressed: () => _changeColor(Colors.blue),
+                  ),
+                ],
               ),
-            ),
+              const SizedBox(height: 30),
+
+              ToggleSwitch(
+                minWidth: 90.0,
+                minHeight: 70.0,
+                initialLabelIndex: _isYellow ? 1 : 0,
+                cornerRadius: 20.0,
+                activeFgColor: Colors.white,
+                inactiveBgColor: Colors.grey,
+                inactiveFgColor: Colors.white,
+                totalSwitches: 2,
+                icons: [
+                  Icons.cloud, // nuvola
+                  Icons.sunny, // sole  scegliete voi l icona preferita
+                ],
+                iconSize: 30.0,
+                activeBgColors: [[Colors.grey], [Colors.yellow]],
+                animate: true,
+                curve: Curves.bounceInOut,
+                onToggle: (index) {
+                  setState(() {
+                    if (index == 0) {
+                      _backgroundColor = Colors.grey;
+                      _isYellow = false;
+                    } else {
+                      _backgroundColor = Colors.yellow;
+                      _isYellow = true;
+                    }
+                  });
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // button reset
+          FloatingActionButton.extended(
+            backgroundColor: Colors.white,
+            onPressed: _resetColor,
+            label: const Text('Reset'),
+            icon: const Icon(Icons.refresh),
+          ),
+          const SizedBox(width: 20),
+          // button random
+          FloatingActionButton.extended(
+            backgroundColor: Colors.white,
+            onPressed: _randomColor,
+            label: const Text('Colore Random'),
+            icon: const Icon(Icons.shuffle),
           ),
         ],
       ),
-      
-      floatingActionButton: 
-      Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        
-        children: [
-        FloatingActionButton(
-          onPressed: clickverde,
-          backgroundColor: Colors.greenAccent,
-          child: const Icon(Icons.color_lens),
-        ),
-        SizedBox(width: 10,),
-        FloatingActionButton(
-          onPressed: clickblu,
-          backgroundColor: Colors.blueAccent,
-          child: const Icon(Icons.color_lens),
-        ),
-        SizedBox(width: 10,),
-        FloatingActionButton(
-          onPressed: clickviola,
-          backgroundColor: Colors.pinkAccent,
-          child: const Icon(Icons.color_lens),
-        ),
-        // SizedBox(width: 10,),
-        // FloatingActionButton( 
-        //   onPressed: _switch,
-        //   child: const Icon(Icons.light_mode),
-  
-        // ),
-        SizedBox(width: 10,),
-        FloatingActionButton(
-          onPressed: _random,
-          backgroundColor: Colors.white,
-          child: const Icon(  Icons.casino)//Text('Random'),
-        ),
-        SizedBox(width: 10,),
-        FloatingActionButton(
-          onPressed: _reset,
-          backgroundColor: Colors.white,
-          child: const Icon(Icons.refresh),
-        ),
-      ],
-      ),
-      
     );
   }
-
-  void _switch() {
-    setState(() {
-      light0 = !light0;
-      if (light0 == true) {
-        _sfondo = const Color.fromARGB(255, 252, 244, 170); //per alleggerire il colore basta cliccare il quadratino colorato e spostarsi in una tonaltà diversa
-      } else {
-        _sfondo = const Color.fromARGB(255, 67, 67, 67);
-      }
-    });
-  }
-  void clickverde () {
-    setState(() {
-      _sfondo = Colors.greenAccent;
-      
-    });
-  }
-  void clickblu () {
-    setState(() {
-      _sfondo = Colors.blueAccent;
-      
-    });
-  }
-  void clickviola () {
-    setState(() {
-      _sfondo = Colors.pinkAccent;
-      
-    });
-  }
-
-  void _random() {
-    final colorirandom = [ // dico i colori che gireranno nel random
-      Colors.greenAccent,
-      Colors.blueAccent,
-      Colors.pinkAccent,
-      Colors.purpleAccent,
-      Colors.cyanAccent,
-      Colors.lightGreenAccent,
-      Colors.amberAccent
-    ];
-    setState(() {
-      //come funziona il random: in questo caso richiamo la variabile dello sfondo(variabile che subirà il cambiamento),
-      //prendiamo la lista dei colori che abbiamo creato
-      //e gli diciamo di sceglie uno tra i colori della lista
-      //l'altro modo di fare un random è utilizzare il Colors.values[Random().nextInt(Colors.values.length)];
-      // il colors.values è una lista che contiene tutti i colori disponibili.
-      _sfondo = colorirandom[Random().nextInt(colorirandom.length)]; 
-    });
-  }
-
-  void _reset() {
-    setState(() {
-       _sfondo = Colors.white;
-    });
-  }
-  
 }
