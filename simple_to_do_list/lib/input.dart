d988d82cd7de3064d7ee28152b1485b74d5a0a3b
Gaia0809 <gaia.mantovani.24@stud.itsaltoadriatico.it>
2025-11-04 12:39:10 +0100

    add checkbox

--- simple_to_do_list/lib/input.dart
+++ simple_to_do_list/lib/input.dart
@@ -2,10 +2,12 @@ class Personal {
   Personal({ // serve per inizializzare le variabili create sotto
     required this.createAt,
     required this.task,
+    this.isDone = false,
     this.expiresAt,
   });
   String task;
   DateTime createAt;
+  bool isDone;
   DateTime? expiresAt;
 }
 
