c5649a331c8fc1959c0319464a3e078b29f7444c
damianocastellarin <damianocastellarin@gmail.com>
2025-11-04 11:31:48 +0100

    refactor main.dart to implement widget for random color and toggle

--- bgcolor_changer/lib/main.dart
+++ bgcolor_changer/lib/main.dart
@@ -6,32 +6,120 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Color _backgroundColor = Colors.white;
+  bool _isLight = true;
+  Brightness brightness = Brightness.light;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Cambio Colore',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme:
+            ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const ColorChangerPage(title: 'Cambio Colore'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Entra nel tuo arcobaleno di idee. Oppure scegli il colore in base al tuo stato d'animo"),
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        ),
+
+        body: AnimatedContainer(
+          duration: const Duration(milliseconds: 500),
+          color: _backgroundColor,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    FloatingActionButton(
+                      backgroundColor: Colors.red,
+                      onPressed: () => _changeColor(Colors.redAccent),
+                    ),
+                    const SizedBox(width: 20),
+                    FloatingActionButton(
+                      backgroundColor: Colors.green,
+                      onPressed: () => _changeColor(Colors.greenAccent),
+                    ),
+                    const SizedBox(width: 20),
+                    FloatingActionButton(
+                      backgroundColor: Colors.blue,
+                      onPressed: () => _changeColor(Colors.blueAccent),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 30),
+
+                ToggleSwitch(
+                  minWidth: 90.0,
+                  minHeight: 70.0,
+                  initialLabelIndex: _isLight ? 1 : 0,
+                  cornerRadius: 20.0,
+                  activeFgColor: Colors.white,
+                  inactiveBgColor: Colors.grey,
+                  inactiveFgColor: Colors.white,
+                  totalSwitches: 2,
+                  icons: [
+                    Icons.dark_mode,
+                    Icons.light_mode,
+                  ],
+                  iconSize: 30.0,
+                  activeBgColors: [
+                    [Colors.black],
+                    [Colors.yellow],
+                  ],
+                  animate: true,
+                  curve: Curves.bounceInOut,
+                  onToggle: (index) {
+                    setState(() {
+                      if (index == 0) {
+                        brightness = Brightness.dark;
+                        _isLight = false;
+                        _backgroundColor = Colors.black;
+                      } else {
+                        brightness = Brightness.light;
+                        _isLight = true;
+                        _backgroundColor = Colors.white;
+                      }
+                    });
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+
+        floatingActionButton: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FloatingActionButton.extended(
+              backgroundColor: Colors.white,
+              onPressed: _resetColor,
+              label: const Text('Reset'),
+              icon: const Icon(Icons.refresh),
+            ),
+            const SizedBox(width: 20),
+            FloatingActionButton.extended(
+              backgroundColor: Colors.white,
+              onPressed: _randomColor,
+              label: const Text('Colore Random'),
+              icon: const Icon(Icons.shuffle),
+            ),
+          ],
+        ),
+      ),
     );
   }
-}
-
-class ColorChangerPage extends StatefulWidget {
-  const ColorChangerPage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<ColorChangerPage> createState() => _ColorChangerPageState();
-}
-
-class _ColorChangerPageState extends State<ColorChangerPage> {
-  Color _backgroundColor = Colors.white;
-  bool _isYellow = true;
 
   void _changeColor(Color newColor) {
     setState(() {
@@ -42,6 +130,7 @@ class _ColorChangerPageState extends State<ColorChangerPage> {
   void _resetColor() {
     setState(() {
       _backgroundColor = Colors.white;
+      brightness = Brightness.light;
     });
   }
 
@@ -55,102 +144,5 @@ class _ColorChangerPageState extends State<ColorChangerPage> {
         ),
       );
     });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-
-      body: AnimatedContainer(
-        duration: const Duration(milliseconds: 500),
-        color: _backgroundColor,
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  // button rosso
-                  FloatingActionButton(
-                    backgroundColor: Colors.red,
-                    onPressed: () => _changeColor(Colors.red),
-                  ),
-                  const SizedBox(width: 20),
-                  // button verde
-                  FloatingActionButton(
-                    backgroundColor: Colors.green,
-                    onPressed: () => _changeColor(Colors.green),
-                  ),
-                  const SizedBox(width: 20),
-                  // button blu
-                  FloatingActionButton(
-                    backgroundColor: Colors.blue,
-                    onPressed: () => _changeColor(Colors.blue),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 30),
-
-              ToggleSwitch(
-                minWidth: 90.0,
-                minHeight: 70.0,
-                initialLabelIndex: _isYellow ? 1 : 0,
-                cornerRadius: 20.0,
-                activeFgColor: Colors.white,
-                inactiveBgColor: Colors.grey,
-                inactiveFgColor: Colors.white,
-                totalSwitches: 2,
-                icons: [
-                  Icons.cloud, // nuvola
-                  Icons.sunny, // sole  scegliete voi l icona preferita
-                ],
-                iconSize: 30.0,
-                activeBgColors: [[Colors.grey], [Colors.yellow]],
-                animate: true,
-                curve: Curves.bounceInOut,
-                onToggle: (index) {
-                  setState(() {
-                    if (index == 0) {
-                      _backgroundColor = Colors.grey;
-                      _isYellow = false;
-                    } else {
-                      _backgroundColor = Colors.yellow;
-                      _isYellow = true;
-                    }
-                  });
-                },
-              ),
-            ],
-          ),
-        ),
-      ),
-
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          // button reset
-          FloatingActionButton.extended(
-            backgroundColor: Colors.white,
-            onPressed: _resetColor,
-            label: const Text('Reset'),
-            icon: const Icon(Icons.refresh),
-          ),
-          const SizedBox(width: 20),
-          // button random
-          FloatingActionButton.extended(
-            backgroundColor: Colors.white,
-            onPressed: _randomColor,
-            label: const Text('Colore Random'),
-            icon: const Icon(Icons.shuffle),
-          ),
-        ],
-      ),
-    );
   }
 }
