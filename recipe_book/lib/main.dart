ee171210d01e3d7ae1bae5ce46f9e00090e89e78
damianocastellarin <damianocastellarin@gmail.com>
2025-11-20 17:52:53 +0100

    son odi fretta non mi viene in mente nessun messagio ma sono avanzato nel lavoro

--- recipe_book/lib/main.dart
+++ recipe_book/lib/main.dart
@@ -1,66 +1,108 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'ricetta.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final List<Ricetta> ricette = [
+    Ricetta(
+      titolo: 'Pasta al Pomodoro',
+      ingredienti: ['- Pasta', '- Pomodoro', '- Olio', '- Sale', '- Basilico'],
+      passaggi: [
+        '- Cuoci la pasta',
+        '- Prepara il sugo con pomodoro e basilico',
+        '- Mescola pasta e sugo',
+        '- Servi caldo'
+      ],
+      url: 'https://www.google.com/search?q=Pasta+al+Pomodoro',
+    ),
+    Ricetta(
+      titolo: 'Tiramisu',
+      ingredienti: ['- Mascarpone', '- Uova', '- Zucchero', '- Caffe', '- Savoiardi'],
+      passaggi: [
+        '- Prepara il caffè',
+        '- Mescola mascarpone, uova e zucchero',
+        '- Fai strati con savoiardi e crema',
+        '- Metti in frigo per 4 ore'
+      ],
+      url: 'https://www.google.com/search?q=Tiramisu',
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      title: 'Ricette',
+      home: Scaffold(
+        appBar: AppBar(title: Text('Ricette')),
+        body: ListView.builder(
+          itemCount: ricette.length,
+          itemBuilder: (context, index) {
+            final ricetta = ricette[index];
+            return ListTile(
+              title: Text(ricetta.titolo),
+              trailing: IconButton(
+                icon: Icon(Icons.link),
+                onPressed: () => _apriLink(ricetta.url),
+              ),
+              onTap: () {
+                showDialog(
+                  context: context,
+                  builder: (context) {
+                    return Dialog(
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              ricetta.titolo,
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            Text('Ingredienti:'),
+                            Text(ricetta.ingredienti[0]),
+                            Text(ricetta.ingredienti[1]),
+                            Text(ricetta.ingredienti[2]),
+                            Text(ricetta.ingredienti[3]),
+                            Text(ricetta.ingredienti[4]),
+                            SizedBox(height: 10),
+                            Text('Passaggi:'),
+                            Text(ricetta.passaggi[0]),
+                            Text(ricetta.passaggi[1]),
+                            Text(ricetta.passaggi[2]),
+                            Text(ricetta.passaggi[3]),
+                            SizedBox(height: 20),
+                            Align(
+                              alignment: Alignment.centerRight,
+                              child: TextButton(
+                                onPressed: () => Navigator.pop(context),
+                                child: Text('Chiudi'),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                );
+              },
+            );
+          },
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
     );
   }
+
+void _apriLink(String url) async {
+  final Uri uri = Uri.parse(url);
+  await launchUrl(uri, mode: LaunchMode.externalApplication);
+}
 }
---
+++ recipe_book/lib/ricetta.dart
@@ -0,0 +1,13 @@
+class Ricetta {
+  String titolo;
+  List<String> ingredienti;
+  List<String> passaggi;
+  String url;
+
+  Ricetta({
+    required this.titolo,
+    required this.ingredienti,
+    required this.passaggi,
+    required this.url,
+  });
+}
